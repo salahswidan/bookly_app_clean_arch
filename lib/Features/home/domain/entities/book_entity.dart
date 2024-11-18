2ff477bc2e71efa443b1649fe0e6e86799cf689b
salah <salahswidan212@gmail.com>
2024-11-18 14:40:15 +0200

    create api servicee

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,9 +1,10 @@
 class BookEntity {
+  final String bookId;
   final String title;
-  final String authorName;
-  final String image;
-  final num price;
-  final num rating;
+  final String? authorName;
+  final String? image;
+  final num? price;
+  final num? rating;
 
-  BookEntity(this.title, this.authorName, this.image, this.price, this.rating);
+  BookEntity({required this.title,required this.authorName,required this.image,required this.price,required this.rating,required this.bookId});
 }
