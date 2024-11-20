60e06d46ac2ce9a5d2f7270df8c275c037b418cf
salah <salahswidan212@gmail.com>
2024-11-20 19:59:10 +0200

    create server failure from dio error

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -4,41 +4,49 @@ import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/Features/home/domain/entities/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
-class HomeRepoImpl  extends HomeRepo{
+class HomeRepoImpl extends HomeRepo {
   final HomeLocalDataSource homeLocalDataSource;
   final HomeRemoteDataSource homeRemoteDataSource;
 
-  HomeRepoImpl({required this.homeLocalDataSource,required this.homeRemoteDataSource});
-  
+  HomeRepoImpl(
+      {required this.homeLocalDataSource, required this.homeRemoteDataSource});
+
   @override
   Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
     try {
       var booksList = await homeLocalDataSource.fetchFeaturedBooks();
-      if(booksList.isNotEmpty){
-        return right(booksList); 
+      if (booksList.isNotEmpty) {
+        return right(booksList);
       }
       var books = await homeRemoteDataSource.fetchFeaturedBooks();
       return right(books);
     } catch (e) {
-      return left(Failure());
-      
+      if (e is DioException) {
+  return left(ServerFailure.fromDioError(e));
+}
+return left(ServerFailure(e.toString()));
+
     }
-    
   }
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async{
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
     try {
-      var booksList = await homeLocalDataSource.fetchNewestBooks();
-      if(booksList.isNotEmpty){
-        return right(booksList); 
+      List<BookEntity> books;
+      books =  homeLocalDataSource.fetchNewestBooks();
+      if (books.isNotEmpty) {
+        return right(books);
       }
-      var books = await homeRemoteDataSource.fetchNewestBooks();
+       books = await homeRemoteDataSource.fetchNewestBooks();
       return right(books);
-    } catch (e) {
-      return left(Failure());
-      
+    }catch (e) {
+      if (e is DioException) {
+  return left(ServerFailure.fromDioError(e));
+}
+return left(ServerFailure(e.toString()));
+
     }
   }
 }
--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,1 +1,43 @@
-class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String message;
+  const Failure(this.message);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.message);
+
+  factory ServerFailure.fromDioError(DioException e) {
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with API server');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with API server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with API server');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad Certificate');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            e.response!.statusCode!, e.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to API server was cancelled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('No Internet Connection');
+      case DioExceptionType.unknown:
+        return ServerFailure('opss something went wrong, please try again');
+    }
+  }
+  factory ServerFailure.fromResponse(int statusCode, dynamic? response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your request not found, please try later!');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal server error, please try later!');
+    } else {
+      return ServerFailure('opss something went wrong, please try again');
+    }
+  }
+}
