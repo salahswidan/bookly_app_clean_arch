a82188790019f7d01810e7722c331b0c7aeb90e6
salah <salahswidan212@gmail.com>
2024-11-21 10:30:00 +0200

    get_it dep injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,74 @@
 import 'package:bookly_app/Features/Splash/presentation/views/splash_view.dart';
+import 'package:bookly_app/Features/home/data/data_sources/home_local_data_source.dart';
+import 'package:bookly_app/Features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly_app/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/Features/home/domain/entities/use_cases/fetch_featured_books_use_case.dart';
+import 'package:bookly_app/Features/home/domain/entities/use_cases/fetch_newest_books_use_case.dart';
+import 'package:bookly_app/Features/home/presentation/manger/featured_book_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/Features/home/presentation/manger/newest_book_cubit/newest_book_cubit.dart';
 import 'package:bookly_app/Features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utlis/api_service.dart';
 import 'package:bookly_app/core/utlis/app_router.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import 'core/utlis/setup_service_locator.dart';
+
 void main() async {
-   await Hive.initFlutter();
+  await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
+  setupServiceLocator();
   await Hive.openBox<BookEntity>(KFeaturedBox);
   await Hive.openBox<BookEntity>(KNewestBox);
   runApp(const MyApp());
- 
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) {
+            return FeaturedBooksCubit(
+              FetchFeaturedBooksUseCase(
+               getIt.get<HomeRepoImpl>(),
+              ),
+            );
+          },
+        ),
+        BlocProvider(
+          create: (context) {
+            return NewestBookCubit(
+              FetchNewestBooksUseCase(
+                             getIt.get<HomeRepoImpl>(),
+
+              ),
+            );
+          },
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
